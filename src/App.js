cc2bd2f3953cd90c195684cf4eeda6a183d258b1
Nicolas de Oliveira <nicolasaquino13@gmail.com>
2022-04-19 20:03:01 -0300

    commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import GitHubImage from './download.png'
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://api.github.com/users/${search}`)
+      .then(response => response.json())
+      .then(userResponse => setUserData(userResponse));
+  }
+
+  console.log(userData);
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+
+  }
+  return (
+    <div className="container text-center">
+      <h1 className='py-5 text-uppercase'>GitHub Profile</h1>
+      <form className='form-group' onSubmit={handleSubmit}>
+        <div className='input-group'>
+          <input type='text'
+            className='form-control'
+            required
+            value={search}
+            onChange={handleChange}
+          />
+          <span className='input-group-btn'>
+            <button type='submit' className='btn btn-success'>
+              Search
+            </button>
+          </span>
+        </div>
+      </form>
+
+      <div className='py-5'>
+        {!userData && (
+          <img
+            src={GitHubImage}
+            className='responsive rounded-circle'
+            height='200px'
+          />
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className='responsive rounded-circle'
+              height='200px'
+            />
+            <h1 className='pt-5'>
+              <a href={userData.html_url} target='_new'>
+                {userData.name}
+              </a>
+            </h1>
+            <h3>
+              {userData.location}
+            </h3>
+            <p>
+              <a href={userData.blog} targe='_new' className='text-info'>
+                {userData.blog}
+              </a>
+            </p>
+
+          </div>
+        )}
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
